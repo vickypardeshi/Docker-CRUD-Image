a4b13777fd00ba2ae24995757a1aaf1db0d39a00
Vicky pardeshi <52427418+vickypardeshi@users.noreply.github.com>
2020-05-10 19:41:41 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -120,5 +120,5 @@ def delete_user(ID):
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port='8080')
                                                                                                                                                                                                                                                                                                              
